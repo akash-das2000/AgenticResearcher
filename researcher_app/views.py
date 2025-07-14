bc31bd7a4baed7ac9dd11ef1a02f6c2d09ab3916
akashdas <44442562+akash-das2000@users.noreply.github.com>
2025-07-14 16:39:26 +0530

    Update views.py

--- researcher_app/views.py
+++ researcher_app/views.py
@@ -1,5 +1,6 @@
 # researcher_app/views.py
 
+#Backend
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
@@ -159,3 +160,14 @@ class NormalizationRuleView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+#Frontend
+from django.shortcuts import render
+
+def upload_page(request):
+    """
+    Renders the upload page for PDFs.
+    """
+    return render(request, 'upload.html')
