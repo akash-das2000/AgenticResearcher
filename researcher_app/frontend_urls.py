c6bdf50ea5e043225bc6d24dd13b46acafb8b97d
akashdas <44442562+akash-das2000@users.noreply.github.com>
2025-07-14 21:05:18 +0530

    Update frontend_urls.py

--- researcher_app/frontend_urls.py
+++ researcher_app/frontend_urls.py
@@ -1,8 +1,18 @@
 # researcher_app/frontend_urls.py
 
 from django.urls import path
-from .views import upload_page
+from .views import (
+    upload_page,
+    chat_page,
+    blog_page,
+    ppt_page,
+    poster_page
+)
 
 urlpatterns = [
     path('upload/', upload_page, name='upload'),
+    path('chat/<int:pk>/', chat_page, name='chat'),
+    path('blog/<int:pk>/', blog_page, name='blog'),
+    path('ppt/<int:pk>/', ppt_page, name='ppt'),
+    path('poster/<int:pk>/', poster_page, name='poster'),
 ]
