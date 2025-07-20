f142d071200b3966a6ee444def25db71457cece2
akashdas <44442562+akash-das2000@users.noreply.github.com>
2025-07-20 09:16:16 +0530

    Create 0002_add_title_to_blogoutline.py

---
+++ researcher_app/migrations/0002_add_title_to_blogoutline.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('researcher_app', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='blogoutline',
+            name='title',
+            field=models.CharField(
+                max_length=200,
+                blank=True,
+                default='',
+                help_text='Final blog title (filled in when ready)'
+            ),
+        ),
+    ]
