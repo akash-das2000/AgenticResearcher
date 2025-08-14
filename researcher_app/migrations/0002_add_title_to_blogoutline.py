12172f2051dc99ac1d92e05b77526583973049fa
akashdas <44442562+akash-das2000@users.noreply.github.com>
2025-08-14 08:11:15 +0530

    Update 0002_add_title_to_blogoutline.py

--- researcher_app/migrations/0002_add_title_to_blogoutline.py
+++ researcher_app/migrations/0002_add_title_to_blogoutline.py
@@ -7,14 +7,15 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='blogoutline',
-            name='title',
-            field=models.CharField(
-                max_length=200,
-                blank=True,
-                default='',
-                help_text='Final blog title (filled in when ready)'
-            ),
-        ),
+        # migrations.AddField(
+        #     model_name='blogoutline',
+        #     name='title',
+        #     field=models.CharField(
+        #         max_length=200,
+        #         blank=True,
+        #         default='',
+        #         help_text='Final blog title (filled in when ready)'
+        #     ),
+        # ),
+        # No-op: 'title' already exists in 0001_initial
     ]
